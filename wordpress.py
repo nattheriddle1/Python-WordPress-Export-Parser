d7b5317fbcacdc9cdb73c0331a5eec620ca1fa48
nattheriddle1 <69092489+nattheriddle1@users.noreply.github.com>
2022-10-11 14:52:43 -0400

    Added Field class
    A descriptor class. Added __init__, __get__, __set_name__, find, transform methods.

--- wordpress.py
+++ wordpress.py
@@ -5,3 +5,35 @@ NAMESPACES = {
     "wfw": "http://wellformedweb.org/CommentAPI/",
     "wp": "http://wordpress.org/export/1.2/",
 }
+
+
+class Field:
+    def __init__(self, key=""):
+        self._key = key
+
+    def __get__(self, obj, klass):
+        if not obj:
+            return self
+        value = self.find(obj)
+        if value is None:
+            return value
+        return self.transform(value)
+
+    def __set_name__(self, klass, name):
+        if not self._key:
+            self._key = "_" + name
+        if self._key.startswith("_"):
+            self._key = ":" + klass.__name__.lower() + self._key
+        if self._key.startswith(":"):
+            self._key = "wp" + self._key
+
+    def find(self, obj):
+        element = obj._tree.find(self._key, NAMESPACES)
+        if element.text is not None:
+            value = element.text.strip()
+            if value:
+                return value
+        return None
+
+    def transform(self, value):
+        return value
