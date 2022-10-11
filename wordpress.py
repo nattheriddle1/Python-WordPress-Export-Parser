9c2364a70d86f2d8f9de99355992b8c9b8722b5a
nattheriddle1 <69092489+nattheriddle1@users.noreply.github.com>
2022-10-11 14:49:02 -0400

    Added XML namespaces constant

---
+++ wordpress.py
@@ -0,0 +1,7 @@
+NAMESPACES = {
+    "excerpt": "http://wordpress.org/export/1.2/excerpt/",
+    "content": "http://purl.org/rss/1.0/modules/content/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "wfw": "http://wellformedweb.org/CommentAPI/",
+    "wp": "http://wordpress.org/export/1.2/",
+}
